2105b54de8409f6b9baa075b44344c81598a1500
SGador <30827601+SGador@users.noreply.github.com>
2017-12-13 18:27:34 +0800

    spacing

--- security.js
+++ security.js
@@ -14,9 +14,7 @@ module.exports = function verifyFacebookSignatureHeader(req, res, buf) {
     var elements = signature.split('sha1=');
     var facebookSignature = elements[1];
 
-    var expectedSignature = crypto.createHmac('sha1', process.env.FB_APP_SECRET)
-      .update(buf)
-      .digest('hex');
+    var expectedSignature = crypto.createHmac('sha1', process.env.FB_APP_SECRET).update(buf).digest('hex');
 
     if (facebookSignature !== expectedSignature) {
       throw new Error("Could not verify message was sent from Facebook.");
